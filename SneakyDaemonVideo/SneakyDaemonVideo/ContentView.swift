73c48d40f4b479509c6b7072820b357fc5b4a8ed
sneakydaemonvideo <sneakydaemonvideo@gmail.com>
2021-01-31 17:22:11 -0800

    updating ContentView.swift with alternative hello message

--- SneakyDaemonVideo/SneakyDaemonVideo/ContentView.swift
+++ SneakyDaemonVideo/SneakyDaemonVideo/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("👋, 🌎 from neglected homework branch 🥳")
             .padding()
     }
 }
